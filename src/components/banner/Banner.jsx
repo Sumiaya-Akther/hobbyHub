7e64e6c7a5b5a1db96e0c47508b6feb5086f8134
Sumiaya Akther <sumiayaakther15@gmail.com>
2025-05-24 17:35:39 +0600

    new

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -5,6 +5,7 @@ import { Carousel } from 'react-responsive-carousel';
 import bannerImg1 from "../../assets/banner-pic-1.jpeg";
 import bannerImg3 from "../../assets/banner-pic-3.jpeg";
 import bannerImg4 from "../../assets/banner-pic-4.jpeg";
+import bannerImg2 from "../../assets/banner-pic-2.jpeg";
 
 const Banner = () => {
     return (
@@ -73,6 +74,33 @@ const Banner = () => {
             {/* Slide 3 */}
             <div className='relative'>
                 <img className='w-full h-[570px] object-cover rounded-2xl' src={bannerImg4} alt="Banner 3" />
+                <div className='absolute  rounded-2xl'></div>
+                <div className='absolute bottom-50 left-1/2 transform -translate-x-1/2 text-center text-black px-4 space-y-4'>
+                     <h1 className="text-4xl font-bold">
+                        Create Your Favourite Hobbies
+                        <br />
+                        <span className="text-indigo-600">
+                            <Typewriter
+                                words={['Explore Groups', 'Join Communities', 'Find Your Passion']}
+                                loop={true}
+                                cursor
+                                cursorStyle="|"
+                                typeSpeed={70}
+                                deleteSpeed={50}
+                                delaySpeed={1000}
+                            />
+                        </span>
+                    </h1>
+                    <p className='font-medium max-w-xl mx-auto'>
+                        Hobbies can provide a welcome break from daily routines <br /> and offer a way to relax and destress.
+                    </p>
+                    <button className="btn bg-purple-500 text-white rounded-2xl px-6 py-2">Create Group</button>
+                </div>
+            </div>
+
+              {/* Slide 4 */}
+            <div className='relative'>
+                <img className='w-full h-[570px] object-cover rounded-2xl' src={bannerImg2} alt="Banner 3" />
                 <div className='absolute  rounded-2xl'></div>
                 <div className='absolute bottom-50 left-1/2 transform -translate-x-1/2 text-center text-black px-4 space-y-4'>
                      <h1 className="text-4xl font-bold">
